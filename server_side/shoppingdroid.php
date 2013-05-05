a6ee66e04c0fdd1a0d02d6496d7a74121069cec4
leonardo7 <leo_aly7@ymail.com>
2013-05-05 05:06:25 +0200

    server side completed

---
+++ server_side/shoppingdroid.php
@@ -0,0 +1,83 @@
+<?php
+
+include_once 'db_config.php';
+include_once 'db_connection.php';
+
+echo "ana hena";
+
+$baseUrl = "http://localhost/shoppingdroid/";
+$db = new DB_CONNECTION();
+$uri = $_SERVER["REQUEST_URI"];
+$path = explode("?", $uri);
+$parts = explode("/", $path[0]."///");
+
+/**
+supported queries 
+product // select product in a specific store
+product_global // select product from all stores
+store // selects a store 
+stores // return all data of stores
+stores_locations // return store name and location(longitude, latitude)
+product_range // select products of same type that are from a threshold from current product price 
+prodcut_range_global // select products of same type that are from a threshold from current product price  from all stores
+*/
+
+$q_type = $parts[2];
+
+echo $q_type."   "."batee5a  ";
+// route to the target method 
+if ($q_type=="product") {
+
+	if (!isset($_GET["barcode"]) || !isset($_GET["store_id"])) 
+		sendResponseAndExit(false, 400, "Bad Request");
+
+	$response = $db->product($_GET["barcode"], $_GET["store_id"]);    
+	sendResponseAndExit(true, 200, "OK", false, $response);
+
+} else if ($q_type=="product_global") {
+
+	if (!isset($_GET["barcode"])) 
+		sendResponseAndExit(false, 400, "Bad Request");
+
+	$response = $db->product_global($_GET["barcode"]);    
+	sendResponseAndExit(true, 200, "OK", false, $response);
+
+} else if ($q_type=="store"){
+
+	if (!isset($_GET["store_id"])) 
+		sendResponseAndExit(false, 400, "Bad Request");
+
+	$response = $db->store($_GET["store_id"]);    
+	sendResponseAndExit(true, 200, "OK", false, $response);
+
+} else if ($q_type=="stores") {
+
+	$response = $db->stores();    
+	sendResponseAndExit(true, 200, "OK", false, $response);
+
+} else if ($q_type=="stores_locations") {
+
+	$response = $db->stores_locations();    
+	sendResponseAndExit(true, 200, "OK", false, $response);
+
+} else if ($q_type=="product_range") {
+
+	if (!isset($_GET["barcode"]) || !isset($_GET["store_id"]) || !isset($_GET["diff_amount"])) 
+		sendResponseAndExit(false, 400, "Bad Request");
+
+	$response = $db->product_range($_GET["barcode"], $_GET["store_id"], $_GET["diff_amount"]);    
+	sendResponseAndExit(true, 200, "OK", false, $response);
+
+} else if ($q_type=="product_range_global") {
+
+	if (!isset($_GET["barcode"]) || !isset($_GET["store_id"]) || !isset($_GET["diff_amount"])) 
+		sendResponseAndExit(false, 400, "Bad Request");
+
+	$response = $db->product_range_global($_GET["barcode"], $_GET["store_id"], $_GET["diff_amount"]);    
+	sendResponseAndExit(true, 200, "OK", false, $response);
+
+} else {
+
+	sendResponseAndExit(false, 404, "Not Found");
+
+}
