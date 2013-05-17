85a2d8446ab297776a308a1fbfa755dba21c5c7e
Nesma Mohssen <nesma.mohssen.khaled@gmail.com>
2013-05-18 01:59:42 +0200

    map working, scanner ready to activate
    take care to chose the appropriate host
    could use a zoom of 2 or 4 to see all the markers

--- src/com/example/shd1/MainActivity.java
+++ src/com/example/shd1/MainActivity.java
@@ -4,6 +4,7 @@ import location.LocationMan;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
@@ -40,10 +41,41 @@ public class MainActivity extends Activity {
 	
 	public void launchScanner(View view){
 	    intent = new Intent(this, ScanToDisplay.class);
-	    // TODO what is that one 
+	    // TODO activate scan by comment the following 2 lines and uncommenting the 4 lines below them
         intent.putExtra(BarCode, "1");
     	startActivity(intent);
+    	
+//      Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+//      intent.putExtra("com.google.zxing.client.android.SCAN.SCAN_MODE", "QR_CODE_MODE");
+//  	Log.i("xZing", "Start Scan");
+//      startActivityForResult(intent, 0);
 	}
+	
+	
+	 public void onActivityResult(int requestCode, int resultCode, Intent intent)
+	    {
+	        if (requestCode == 0)
+	        {
+	        	Log.i("xZing", "Back From Scanning");
+	            if (resultCode == RESULT_OK)
+	            {
+	            	Log.i("xZing", "Scan Successful");
+	                String contents = intent.getStringExtra("SCAN_RESULT");
+	                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+	             //   Log.i("xZing", "contents: "+contents+" format: "+format);
+	                Intent intent2 = new Intent(this, ScanToDisplay.class);
+	                intent2.putExtra(BarCode, contents);
+	            	startActivity(intent2);
+
+	                // Handle successful scan
+	            }
+	            else if (resultCode == RESULT_CANCELED)
+	            {
+	                // Handle cancel
+	               // Log.i("xZing", "Cancelled");
+	            }
+	        }
+	    }
 	
 	 @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
