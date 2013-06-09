c093677a4db8f9d911d085f4a677fa32f909dfab
leonardo7 <leo_aly7@ymail.com>
2013-06-09 21:35:03 +0200

    removing getStores.java

--- src/com/shoppingDroid/main/MapsActivity.java
+++ src/com/shoppingDroid/main/MapsActivity.java
@@ -8,7 +8,6 @@ import org.json.JSONObject;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
