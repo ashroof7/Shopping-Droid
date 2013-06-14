5c0c455d3cce65a9cb81f08832f34fbfc5a4dd0c
leonardo7 <leo_aly7@ymail.com>
2013-06-14 13:06:13 +0200

    updated icons, server

--- server_side/db_connection.php
+++ server_side/db_connection.php
@@ -41,9 +41,9 @@ class DB_CONNECTION {
    /**
      * Function to connect with database
      */
-   function connect() {
+function connect() {
 
-        // import database connection variables
+    // import database connection variables
     require_once __DIR__ . '/db_config.php';
     $this->db = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD,DB_DATABASE)  or die("Connect failed : ".mysqli_connect_error() );
     
@@ -77,16 +77,16 @@ class DB_CONNECTION {
     $this->product_range_pivot_stmt->bind_param('si',$this->barcode_ ,$this->storeid);      
     
 
-    $this->product_range_stmt = $this->db->prepare("select product_name, price
+    $this->product_range_stmt = $this->db->prepare("select product_barcode, product_name, price
         from product p, stores, product_type, product_store ps
         where p.product_id = ps.product_id  and type_id = product_type and ps.store_id = stores.store_id 
         and stores.store_id = ? and type_id = ? and price between ? - ? and ? + ?;");
 
-    $this->product_range_stmt->bind_param('iidddd',$this->storeid ,$this->pivot_type,
+    $this->product_range_stmt->bind_param('iidddd', $this->storeid ,$this->pivot_type,
         $this->pivot_price, $this->diffamount, $this->pivot_price, $this->diffamount);      
     
     
-    $this->product_range_global_stmt = $this->db->prepare(" select product_name, price, store_name
+    $this->product_range_global_stmt = $this->db->prepare(" select product_barcode, product_name, price, store_name
         from product p, stores, product_type, product_store ps
         where p.product_id = ps.product_id  and type_id = product_type and ps.store_id = stores.store_id 
         and type_id = ? and price between ? - ? and ? + ?;");
@@ -160,6 +160,7 @@ function product_global($barcode){
             $currstore["product_price"] = $price;
             array_push($productdata["stores"],$currstore);                   
         }            
+        $productdata["product_barcode"] = $barcode;
         $productdata["product_name"] = $p_name;
         $productdata["product_type"] = $type_name;
 
@@ -177,7 +178,7 @@ function product_global($barcode){
 
 function product($barcode, $store_id){
     if($this->product_stmt){
-        
+
         $this->barcode_= $barcode;
         $this->storeid = $store_id; 
         $this->product_stmt->execute();
@@ -221,6 +222,7 @@ function stores(){
             $currstore["store_name"] = $store_name;
             $currstore["latitude"] = $latitude;
             $currstore["longitude"] = $longitude;
+
             $currstore["store_address"] = $store_address;
             array_push($res["stores"],$currstore);                   
         }
@@ -284,14 +286,15 @@ function product_range($barcode, $store_id, $diff_amount){
         $this->product_range_pivot_stmt->close();
         
         $this->product_range_stmt->execute();
-        $this->product_range_stmt->bind_result($p_name , $price);
+        $this->product_range_stmt->bind_result($p_barcode, $p_name , $price);
         
         $res = array();
         $res["products"] = array();
         
         $currproduct = array();
         while($this->product_range_stmt->fetch()) 
-        {
+        {   
+            $currproduct["product_barcode"] = $p_barcode;
             $currproduct["product_name"] = $p_name;
             $currproduct["product_price"] = $price;
             array_push($res["products"],$currproduct);                   
@@ -307,10 +310,10 @@ function product_range($barcode, $store_id, $diff_amount){
 }       
 
 function product_range_global($barcode, $store_id, $diff_amount){
-    
+
     if($this->product_range_global_stmt) 
     {
-        
+
         $this->barcode_ = $barcode;
         $this->storeid = $store_id;
         
@@ -324,7 +327,7 @@ function product_range_global($barcode, $store_id, $diff_amount){
         $this->product_range_pivot_stmt->close();
 
         $this->product_range_global_stmt->execute();
-        $this->product_range_global_stmt->bind_result($p_name , $price, $store_name);
+        $this->product_range_global_stmt->bind_result($p_barcode, $p_name , $price, $store_name);
 
         $res = array();
         $res["products"] = array();
@@ -334,9 +337,9 @@ function product_range_global($barcode, $store_id, $diff_amount){
         while($this->product_range_global_stmt->fetch()) 
         {
             $currproduct["store_name"] = $store_name; 
+            $currproduct["product_barcode"] = $p_barcode; 
             $currproduct["product_name"] = $p_name;
             $currproduct["product_price"] = $price;
-
             array_push($res["products"],$currproduct);                   
         }
         $result = json_encode($res, JSON_PRETTY_PRINT);
@@ -351,35 +354,36 @@ function product_range_global($barcode, $store_id, $diff_amount){
 
 
 function stores_in_range($latitude_ , $longitude_ , $range){
- if ($this->stores_in_range_stmt)
- {
-     $this->longitude = $longitude_ ;
-     $this->latitude = $latitude_ ;
-     $this->radius = $range ;
-     $this->stores_in_range_stmt->execute();
-     $this->stores_in_range_stmt->bind_result($store_id, $store_name , $latitude, $longitude, $store_address);
-     
-     $res = array();
-     $res["stores"] = array();   
-     $currstore = array();
-     
-     while($this->stores_in_range_stmt->fetch())
-     {
+
+   if ($this->stores_in_range_stmt)
+   {
+       $this->longitude = $longitude_ ;
+       $this->latitude = $latitude_ ;
+       $this->radius = $range ;
+       $this->stores_in_range_stmt->execute();
+       $this->stores_in_range_stmt->bind_result($store_id, $store_name , $latitude, $longitude, $store_address);
+
+       $res = array();
+       $res["stores"] = array();   
+       $currstore = array();
+
+       while($this->stores_in_range_stmt->fetch())
+       {
         $currstore["store_id"] = $store_id;
         $currstore["store_name"] = $store_name;
         $currstore["latitude"] = $latitude;
         $currstore["longitude"] = $longitude;
         $currstore["store_address"] = $store_address;
         array_push($res["stores"],$currstore);                   
-    }
+        }
 
     $result = json_encode($res, JSON_PRETTY_PRINT);
     $this->stores_in_range_stmt->close(); 
     return $result;
     
-} else {
-    die('Query failed: ' . mysqli_error($this->DB));
-}
+    } else {
+        die('Query failed: ' . mysqli_error($this->DB));
+    }
 
 }
 
